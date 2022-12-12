28dd64c28cf4847ad6494753c0f8d4599911dad7
Josephshitandi <jsphshtnd@gmail.com>
2022-12-12 20:18:07 +0300

    Modified projectapi

--- job_search/urls.py
+++ job_search/urls.py
@@ -14,8 +14,37 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.contrib.auth import views
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="JOB SEARCH APIs",
+      default_version='v1',
+      description="APIs for the Job Search Application",
+      terms_of_service="https://job-search/policies/terms/",
+      contact=openapi.Contact(email="contact@job-search.local"),
+      license=openapi.License(name="TEST License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('job_app.urls')),
+    path('tinymce/', include('tinymce.urls')),
+    path('api-token-auth/', obtain_auth_token),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+
+admin.site.index_title="JOB SEARCH APP"
+admin.site.site_header="JOB SEARCH APP Admin"
+admin.site.site_title="JOB SEARCH APP"
