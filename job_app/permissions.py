02af529b56d3baec1b3daa8946cd42a146ad4871
Josephshitandi <jsphshtnd@gmail.com>
2022-12-13 23:22:21 +0300

    Added IsAdminOrReadOnly class

--- job_app/permissions.py
+++ job_app/permissions.py
@@ -1,3 +1,12 @@
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        else:
+            return request.user.is_staff
+
 class IsAdmin(BasePermission): 
     def has_permission(self, request, view):
         admin = request.user.user_type ==3
